2424d315988ebc74fc025e022b5e26a34cdb2630
Blaz <mail@blaz.link>
2016-03-02 16:40:55 +0100

    Basic project setup

---
+++ src/LASsie.h
@@ -0,0 +1,17 @@
+#ifndef LASSIE_LASSIE_H
+#define LASSIE_LASSIE_H
+
+namespace modri
+{
+	class LASsie
+	{
+		private:
+			
+
+		public:
+			LASsie();
+			virtual ~LASsie();
+	};
+}
+
+#endif // LASSIE_LASSIE_H
---
+++ testsrc/main.cpp
@@ -0,0 +1,8 @@
+#include "../src/LASsie.h"
+
+int main(int argc, char **argv)
+{
+    
+    
+    return 0;
+}
